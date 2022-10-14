2382ad249edbd3094727a29b6d5b250e0ab150ec
Robo Max <mitrusinmaksim@gmail.com>
2022-10-14 19:43:57 +0300

    Normal_motor_control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomBeta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomBeta.java
@@ -6,20 +6,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
 public class AutonomBeta extends LinearOpMode {
-    private DcMotor leftFrontDrive = null;
-    private PidRegulator PID=new PidRegulator(0.025,0.0000001,0.001);
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        PID.target = 90*4;
-        while (opModeIsActive()) {
-            double power= PID.update(leftFrontDrive.getCurrentPosition());
-            leftFrontDrive.setPower(power);
-            telemetry.addData("target-current position",PID.target- leftFrontDrive.getCurrentPosition());
-            telemetry.addData("power",power);
-            telemetry.update();
-        }
-    }
+    DriveTrain driveTrain;
+ public void runOpMode() {
+     driveTrain=new DriveTrain(hardwareMap);
+     waitForStart();
+     driveTrain.setMotor(100,0,0);
+ }
 }
