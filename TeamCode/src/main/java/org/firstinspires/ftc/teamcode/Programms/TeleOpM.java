4f0860f35d32cc62d772676f4ef703bbdd032e06
MrFrosty1234 <mitrusinmaksim@gmail.com>
2022-12-06 20:45:00 +0300

    Autonomus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Programms/TeleOpM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Programms/TeleOpM.java
@@ -7,6 +7,7 @@ import org.firstinspires.ftc.teamcode.Robot.DriveTrain;
 import org.firstinspires.ftc.teamcode.Robot.Graber;
 import org.firstinspires.ftc.teamcode.Robot.Lift;
 import org.firstinspires.ftc.teamcode.Robot.Lightning;
+import org.firstinspires.ftc.teamcode.Robot.Odometry;
 
 @TeleOp
 public class TeleOpM extends LinearOpMode {
@@ -22,29 +23,36 @@ public class TeleOpM extends LinearOpMode {
     Graber graber;
     Lift lift;
     Lightning lightning;
-
+    Odometry odometry;
     @Override
     public void runOpMode() throws InterruptedException {
         driveTrain = new DriveTrain(hardwareMap, this);
-        graber = new Graber(hardwareMap);
+        /*graber = new Graber(hardwareMap);
         lightning = new Lightning(hardwareMap);
         lift = new Lift(hardwareMap, this);
+
+         */
+        odometry=new Odometry(this);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
-        lightning.lightMode = Lightning.LightningMode.SMOOTH;
+       /* lightning.lightMode = Lightning.LightningMode.SMOOTH;
         lift.liftPosition = Lift.LiftPosition.ZERO;
+
+        */
         while (opModeIsActive()) {
             boolean square = gamepad1.square;
-            if (square && !oldsquare) {
+            /*if (square && !oldsquare) {
                 graberPosition = !graberPosition;
             }
+
+             */
             boolean circle = gamepad1.circle;
-            graber.Target_Graber(graberPosition);
+           // graber.Target_Graber(graberPosition);
             boolean triangle = gamepad1.triangle;
             boolean cross = gamepad1.cross;
-            if(gamepad1.dpad_down) {
+          /*  if(gamepad1.dpad_down) {
                 lift.liftMode = Lift.LiftMode.AUTO;
                 lift.liftPosition = Lift.LiftPosition.ZERO;
             }
@@ -74,17 +82,24 @@ public class TeleOpM extends LinearOpMode {
             if (!triangle && !cross) {
                 lift.power = 0;
             }
-
+            odometry.update();
             lift.update();
 
-            lightning.update();
+           */
+            odometry.update();
+            telemetry.addData("x",odometry.x);
+            telemetry.addData("y",odometry.y);
+            driveTrain.displayEncoders();
+            telemetry.addData("heading",odometry.heading);
+
+           // lightning.update();
 
             double axial = -gamepad1.left_stick_y * speed;
             double lateral = -gamepad1.left_stick_x * speed;
             double yaw = -gamepad1.right_stick_x * speed;
 
 
-            if (gamepad1.right_trigger > 0.1 || (lift.encoders() > 130 && axial < 0)) {
+            /*if (gamepad1.right_trigger > 0.1 || (lift.encoders() > 130 && axial < 0)) {
                 if (lift.encoders() > 700 && axial < 0) {
                     axial /= 4.5;
                     lateral /= 4.5;
@@ -97,11 +112,13 @@ public class TeleOpM extends LinearOpMode {
 
             }
 
+
+             */
             driveTrain.setPowers(axial, lateral, yaw);
             oldsquare = square;
             oldcircle = circle;
             oldtriangle = triangle;
-            driveTrain.positionsEncodersXY();
+            //driveTrain.positionsEncodersXY();
             telemetry.update();
         }
         }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Odometry.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+public class Odometry {
+    BNO055IMU gyro;
+
+    double crr = 24 * 20 / (9.8 * PI);
+
+    private DcMotor left_front_drive;
+    private DcMotor left_back_drive;
+    private DcMotor right_front_drive;
+    private DcMotor right_back_drive;
+    public double x = 0, y = 0, heading = 0;
+
+    double lfdold = 0;
+    double lbdold = 0;
+    double rfdold = 0;
+    double rbdold = 0;
+    LinearOpMode linearOpMode1;
+    public Odometry(LinearOpMode linearOpMode) {
+        linearOpMode1=linearOpMode;
+        gyro = linearOpMode.hardwareMap.get(BNO055IMU.class, "imu");
+
+        left_front_drive = linearOpMode.hardwareMap.dcMotor.get("left_front_drive");
+        left_back_drive = linearOpMode.hardwareMap.dcMotor.get("left_back_drive");
+        right_front_drive = linearOpMode.hardwareMap.dcMotor.get("right_front_drive");
+        right_back_drive = linearOpMode.hardwareMap.dcMotor.get("right_back_drive");
+
+
+        left_front_drive.setDirection(DcMotor.Direction.FORWARD);
+        left_back_drive.setDirection(DcMotor.Direction.FORWARD);
+        right_front_drive.setDirection(DcMotor.Direction.REVERSE);
+        right_back_drive.setDirection(DcMotor.Direction.REVERSE);
+
+
+    }
+
+    public void update() {
+        int lfd = left_front_drive.getCurrentPosition();
+        int lbd = left_back_drive.getCurrentPosition();
+        int rfd = right_front_drive.getCurrentPosition();
+        int rbd = right_back_drive.getCurrentPosition();
+        double angle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+
+        double deltaX = xPosition(lfd - lfdold, lbd - lbdold, rfd - rfdold, rbd - rbdold);
+        double deltaY = yPosition(lfd - lfdold, lbd - lbdold, rfd - rfdold, rbd - rbdold);
+
+        x += deltaX * cos(toRadians(-angle)) + deltaY * sin(toRadians(-angle));
+        y += -deltaX * sin(toRadians(-angle)) + deltaY * cos(toRadians(-angle));
+        linearOpMode1.telemetry.addData("deltaX",deltaX);
+        linearOpMode1.telemetry.addData("deltaY",deltaY);
+        heading = angle;
+        lfdold = lfd;
+        lbdold = lbd;
+        rfdold = rfd;
+        rbdold = rbd;
+    }
+
+    public double xPosition(double lfd, double lbd, double rfd, double rbd) {
+        double x = ((lfd + lbd + rfd + rbd) / 4) / crr;
+        return x;
+    }
+
+    public double yPosition(double lfd, double lbd, double rfd, double rbd) {
+        double y = ((-lfd + lbd + rfd - rbd) / 4) / crr;
+        return y;
+    }
+}
